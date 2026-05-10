56734593802d814e70eabf816bd00e254ead32d9
Ralph <gonzagaralphdainiell@gmail.com>
2026-05-10 23:43:29 +0800

    Arrays

--- practices/Day12.swift
+++ practices/Day12.swift
@@ -1,13 +1,47 @@
-let userName: String = "Ralph"
-var address: String? = nil 
+var downloads: [String] = ["AppIcon.png", "Resume.pdf", "UIAssets.zip", "Resume.pdf", "SwiftGuide.pdf"]
 
-print("User: \(userName)")
-address = "Cavite"
+downloads.append("Portfolio.zip") 
+downloads.insert("ProfilePhoto.jpg", at: 0)
 
-if let location = address {
-    print("Address: \(location)")
+print("Total Downloads: \(downloads.count)")
+
+
+print("First Download: \(downloads.first ?? "No First Download")")
+print("Last Download: \(downloads.last ?? "No last Download")")
+
+print("Resume.pdf exists: \(downloads.contains("Resume.pdf"))")
+
+
+if let find = downloads.firstIndex(of: "Resume.pdf") {
+    print("First Resume.pdf at index: \(find)")
+    
 }else {
-    print("Address: No address")
+    print("Can't find the index")
+    
 }
 
-//end 
+for DL in downloads {
+
+    if DL == "Resume.pdf" {
+        print("Duplicate Download: \(DL)")
+        
+    }else {
+        print("Download: \(DL)")
+        
+    }
+}
+
+
+if let remove = downloads.firstIndex(of: "Resume.pdf") {
+    downloads.remove(at: remove)
+}
+
+print("Resume.pdf still exists: \(downloads.contains("Resume.pdf"))")
+
+print("Updated Count: \(downloads.count)")
+
+
+for (index,DownLoads) in downloads.enumerated() {
+    print("[\(index)] \(DownLoads)")
+    
+}

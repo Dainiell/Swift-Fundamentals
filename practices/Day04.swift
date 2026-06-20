a36b87dacd190bd9dbf4029254fa13445f3d9e75
Ralph <gonzagaralphdainiell@gmail.com>
2026-06-20 23:51:10 +0800

    Dictionaries in Swift

--- practices/Day04.swift
+++ practices/Day04.swift
@@ -1,18 +1,17 @@
-var language = "English"
-language = "Tagalog"
-language = "Japanese"
-language = "Spanish"
+var weeklyTemperatures: [String: Int] = [:] 
+weeklyTemperatures = ["Monday": 70, "Tuesday": 75, "Wednesday": 80, "Thursday": 85, "Friday": 90, "Saturday": 95]
+weeklyTemperatures ["Monday"]! += 20 
+print("The Temperature on Monday is \(weeklyTemperatures["Monday"]!)°F")
 
-if language == "English" {
-    print("The Default Language is set to English")
-    
-}else if language == "Tagalog" {
-    print("Ang gamit mo ngayon ay tagalog language")
-    
-}else if language == "Japanese" {
-    print("現在の言語設定は日本語です")
-    
-}else {
-    print("Please Select Language")
-    
+if let temperature = weeklyTemperatures["Sunday"] {
+    print("Sunday: \(temperature)°F")
+} else {
+    weeklyTemperatures["Sunday"] = 100
+    print("The temperature on Sunday is \(weeklyTemperatures["Sunday"]!)°F")
+}
+
+if weeklyTemperatures.count == 7 {
+    print("This week's weather forecast is complete with all 7 days.")
+    weeklyTemperatures = [:]
+    print("The weekly temperatures dictionary has been reset for next week's forecast.")
 }

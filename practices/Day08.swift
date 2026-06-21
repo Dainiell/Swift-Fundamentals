a16cff9beb6a9d7ef672ff6ef620c6fa62653fc8
Ralph <gonzagaralphdainiell@gmail.com>
2026-06-21 14:39:43 +0800

    Closure Function with Swift

--- practices/Day08.swift
+++ practices/Day08.swift
@@ -1,59 +1,7 @@
-// Forced Unwrapping
-var password = "1234"
-let passcode = Int(password)
-print("The passcode of the app is \(passcode!)")
-
-
-
-
-
-
-// Optional Binding
-password = "Hello World"
-if let code = Int(password) {
-    print("Your Password is \(code)")
-} else {
-    print("Invalid passcode!")
+var goldBars = 0
+let unlockTreasureChest: (inout Int) -> Void = { inventory in
+    inventory += 100
 }
 
-
-
-
-
-// Optional Binding with Default Value
-let accessCode: Int
-if let code = Int(password) {
-    accessCode = code
-} else {
-    accessCode = 1111
-}
-print("The passcode of the app is \(accessCode)")
-
-
-
-
-// Multiple Optional Bindings
-let firstPassword = "hello"
-let secondPassword = "world"
-
-if let firstPasscode = Int(firstPassword), let secondPasscode = Int(secondPassword) {
-    print("The first passcode of the app is \(firstPasscode) and the second passcode of the app is \(secondPasscode).")
-} else {
-    print("Invalid passcodes!")
-}
-
-
-
-
-// Multiple Optional Bindings with Default Value
-let firstAccessCode: Int
-let secondAccessCode: Int
-
-if let firstPasscode = Int(firstPassword), let secondPasscode = Int(secondPassword) {
-    firstAccessCode = firstPasscode
-    secondAccessCode = secondPasscode
-} else {
-    firstAccessCode = 1111
-    secondAccessCode = 2222
-}
-print("The first passcode of the app is \(firstAccessCode) and the second passcode of the app is \(secondAccessCode).")
+unlockTreasureChest(&goldBars)
+print(goldBars)

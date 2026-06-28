5e4bcc75dacc3dea7d2d84d872c9d2e886e021c7
Ralph <gonzagaralphdainiell@gmail.com>
2026-06-28 15:02:48 +0800

    Keep going!

--- practices/Day04.swift
+++ practices/Day04.swift
@@ -1,17 +1,16 @@
-var weeklyTemperatures: [String: Int] = [:] 
-weeklyTemperatures = ["Monday": 70, "Tuesday": 75, "Wednesday": 80, "Thursday": 85, "Friday": 90, "Saturday": 95]
-weeklyTemperatures ["Monday"]! += 20 
-print("The Temperature on Monday is \(weeklyTemperatures["Monday"]!)°F")
+let levels = 10 
+let freeLevels = 4
+let bonusLevels = 3
 
-if let temperature = weeklyTemperatures["Sunday"] {
-    print("Sunday: \(temperature)°F")
-} else {
-    weeklyTemperatures["Sunday"] = 100
-    print("The temperature on Sunday is \(weeklyTemperatures["Sunday"]!)°F")
-}
-
-if weeklyTemperatures.count == 7 {
-    print("This week's weather forecast is complete with all 7 days.")
-    weeklyTemperatures = [:]
-    print("The weekly temperatures dictionary has been reset for next week's forecast.")
+for levels in 1...levels {
+    if levels == bonusLevels {
+        print("Skip the level \(bonusLevels)")
+        continue
+    }
+    print("Play level \(levels)")
+    if levels == freeLevels {
+        print("You have played all \(freeLevels) free levels. Buy the game to play the remaining \(levels - freeLevels) levels.")
+        break
+    }
+    
 }

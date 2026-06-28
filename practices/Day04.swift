3123cc47b394be7c5376565c24a318a42998a12c
Ralph <gonzagaralphdainiell@gmail.com>
2026-06-28 18:16:07 +0800

    Keep Going

--- practices/Day04.swift
+++ practices/Day04.swift
@@ -2,13 +2,13 @@ let levels = 10
 let freeLevels = 4
 let bonusLevels = 3
 
-for levels in 1...levels {
-    if levels == bonusLevels {
+for level in 1...levels {
+    if level == bonusLevels {
         print("Skip the level \(bonusLevels)")
         continue
     }
     print("Play level \(levels)")
-    if levels == freeLevels {
+    if level == freeLevels {
         print("You have played all \(freeLevels) free levels. Buy the game to play the remaining \(levels - freeLevels) levels.")
         break
     }

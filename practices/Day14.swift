8e45048a57af87eb78cc82d2103c3c694b597543
Ralph <gonzagaralphdainiell@gmail.com>
2026-07-05 15:29:08 +0800

    Keep going

--- practices/Day14.swift
+++ practices/Day14.swift
@@ -1,76 +1,89 @@
-struct BankAccount {
-    var debitBalance = 0
-    var creditBalance = 0
-    let creditLimit = 100
+let studentName: String = "Ralph"
+let University: String = "LPU"
+var logIn: Bool = false 
+logIn.toggle() 
+
+if logIn == true {
+    print("Welcome \(studentName) from \(University)!")
     
-    var debitBalanceInfo: String {
-        return "Debit balance: $\(debitBalance)"
-    }
+}else {
+    print("Please login first!")
     
-    var availableCredit: Int {
-        return creditLimit + creditBalance
-    }
-    
-    var creditBalanceInfo: String {
-        return "Available credit: $\(availableCredit)"
-    }
-    
-    func debitDeposit(_ amount: Int) {
-        var balance = debitBalance
-        balance += amount
-        if amount > 0 {
-            print("Debit deposit: $\(amount). \(debitBalanceInfo)")
-        }
-    }
-    
-    mutating func creditDeposit(_ amount: Int) {
-        creditBalance += amount
-        print("Credit deposit: $\(amount). \(creditBalanceInfo)")
-        if creditBalance == 0 {
-            print("Paid off credit balance.")
-        } else if creditBalance > 0 {
-            print("Overpaid credit balance.")
-        }
-    }
-    
-    mutating func debitWithdraw(_ amount: Int) {
-        if amount > debitBalance {
-            print("Insufficient funds to withdraw $\(amount). \(debitBalanceInfo)")
-        } else {
-            debitBalance -= amount
-            print("Debit withdraw: $\(amount). \(debitBalanceInfo)")
-        }
-    }
-    
-    mutating func creditWithdraw(_ amount: Int) {
-        if amount > availableCredit {
-            print("Insufficient credit to withdraw $\(amount). \(creditBalanceInfo)")
-        } else {
-            creditBalance -= amount
-            print("Credit withdraw: $\(amount). \(creditBalanceInfo)")
-        }
-    }
-    
-    mutating func debitDeposit(amount: Int) {
-        debitBalance += amount
-        print("Debit deposit: $\(amount). \(debitBalanceInfo)")
+}
+
+var subjects: [String:Int] = [:] 
+
+subjects ["Intro Swift Programming"] = 97
+subjects ["SwiftUi Programming"] = 95
+subjects ["Data Security"] = 93
+subjects ["Intro to DevOps"] = 94 
+ 
+
+print("Total subjects: \(subjects.count)")
+
+subjects["Data Security"]! += 3 
+print("Updated Data Security score: \(subjects["Data Security"]!)")
+
+
+if let subs = subjects["Filipino"] {
+   print("Filipino score: \(subs)")
+}else {
+    subjects ["Filipino"] = 80
+    print("Filipino added with score: \(subjects["Filipino"]!) ")
+}
+
+
+var subsScore: [Int] = []
+
+subsScore.append(97)
+subsScore.append(95)
+subsScore.append(96)
+subsScore.append(94)
+
+
+print("Total scores recorded: \(subsScore.count)")
+
+
+for (number,subsScore) in subsScore.enumerated() {
+    if subsScore >= 90 {
+        print("Subjet \(number + 1): \(subsScore) Outstanding")
+        
+    }else if subsScore <= 89 && subsScore >= 75 {
+        print("Subjet \(number + 1): \(subsScore) Passed")
+        
+    }else if subsScore >= 60 && subsScore <= 74 {
+        print("Subjet \(number + 1): \(subsScore) Needs Improvement")
+    }else  {
+        print("Failed")
+        
     }
 }
 
-var bankAccount = BankAccount()
+var total = 0
 
-print(bankAccount.debitBalanceInfo)
+for score in subsScore {
+    total += score
+}
+print("Total score: \(total)")
 
-bankAccount.debitDeposit(amount: 100)
+let average = total / subsScore.count
+switch average {
+  case 90...100 :print("Average: \(average) Outstanding")
+  case 75...89 : print("Average: \(average) Good Job")
+  case 60...74 : print("Average: \(average) Keep studying")
+  default: print("At risk")
+  
+}
 
-bankAccount.debitWithdraw(20)
-bankAccount.debitWithdraw(81)
+var Students: (name: String , averages: Int) = ("Ralph" , 95)
 
-print(bankAccount.creditBalanceInfo)
+if Students.averages >= 75 {
+     print("\(Students.name) passed with an average of \(Students.averages)")
+}
 
-bankAccount.creditWithdraw(101)
-bankAccount.creditWithdraw(100)
+var pendings: [String] = ["Review Swift Optionals","Review Swift Arrays","Review Swift Dictionaries"]
 
-bankAccount.creditDeposit(50)
-bankAccount.creditDeposit(50)
-bankAccount.creditDeposit(50)
+repeat {
+    print(pendings.last!)
+    pendings.removeLast()
+}while !pendings.isEmpty

d20a955f20f97214592ede735968c4a9fe9171fd
Ralph <gonzagaralphdainiell@gmail.com>
2026-07-05 12:33:59 +0800

    keep going

--- practices/Day13.swift
+++ practices/Day13.swift
@@ -1,39 +1,50 @@
-class VirtualBankSystem {
-    var accountType: String = ""
+let credentials = ("",-1111) 
+
+if credentials.0 == "" || credentials.1 < 0 {
+    print("Invalid Credentials!")
     
-    func welcomeCustomer() {
-        print("Welcome to your virtual bank system.")
-    }
+}else {
+    print("The username is \(credentials.0) and the passcode is \(credentials.1)")
     
-    func onboardCustomerAccountOpening() {
-        print("What kind of account would you like to open?")
-        print("1. Debit account")
-        print("2. Credit account")
-    }
-    
-    func makeAccount(numberPadKey: Int) {
-        print("The selected option is \(numberPadKey)")
-        
-        switch numberPadKey {
-        case 1:
-            accountType = "debit"
-        case 2:
-            accountType = "credit"
-        default:
-            print("Invalid input: \(numberPadKey)")
-        }
-        
-        if accountType != "" {
-            print("You have opened a \(accountType) account.")
-        }
-    }
 }
 
-let virtualBankSystem = VirtualBankSystem()
-virtualBankSystem.welcomeCustomer()
+let fullCredentials = (password:"pass",passcode:1111)
 
-repeat {
-    virtualBankSystem.onboardCustomerAccountOpening()
-    let numberPadKey = Int.random(in: 1...3)
-    virtualBankSystem.makeAccount(numberPadKey: numberPadKey)
-} while virtualBankSystem.accountType == ""
+if fullCredentials.password == "" || fullCredentials.passcode < 0 {
+    print("Invalid Crendetials!")
+    
+}else {
+    print("The Password is \(fullCredentials.password) and the passcode is \(fullCredentials.passcode)")
+    
+}
+
+
+
+var weeklyTemperatures: [String: Int] = [:]
+
+weeklyTemperatures = ["Monday": 70, "Tuesday": 75, "Wednesday": 80, "Thursday": 85, "Friday": 90, "Saturday": 95] 
+weeklyTemperatures ["Monday"]! += 20
+print("The temperature on Monday is \(weeklyTemperatures["Monday"]!)°F.")
+
+if let temperature = weeklyTemperatures["Sunday"] {
+    print("The temperature on sunday is \(temperature)°F.")
+    
+}else {
+    weeklyTemperatures["Sunday"] = 100 
+    print("The temperature on Sunday is \(weeklyTemperatures["Sunday"]!)°F.")
+    
+}
+
+let counts = weeklyTemperatures.count 
+print(counts)
+
+if counts == 7 {
+    print("All the temperatures are complete")
+    weeklyTemperatures = [:]
+    print("Ready for next week's weather forecast.")
+}else {
+    print("Some temperatures are missing")
+    
+}
+
+

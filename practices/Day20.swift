589d79d7296be8e312bb81b6cd5fb9df455b736e
Ralph <gonzagaralphdainiell@gmail.com>
2026-07-15 22:46:09 +0800

    keep going

--- practices/Day20.swift
+++ practices/Day20.swift
@@ -11,6 +11,7 @@ print(applyOperation(number: 5) {$0 * 3})
 
 
 
+
 var isCorrectAnswer = { (userAnswer: String, correctAnswer: String) -> Bool in
     return userAnswer == correctAnswer 
     
@@ -18,3 +19,13 @@ var isCorrectAnswer = { (userAnswer: String, correctAnswer: String) -> Bool in
 print(isCorrectAnswer("Manila","Manila"))
 
 
+
+
+func forwards(s1:String,s2:String) -> Bool {
+    return s1 > s2
+}
+let letters = ["H","E","L","L","O"]
+print(letters)
+
+let sorted = letters.sorted(by: forwards)
+print(sorted)

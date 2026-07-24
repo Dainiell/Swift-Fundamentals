f13820608cdaf0e243ef113b20165d468669c343
Ralph <gonzagaralphdainiell@gmail.com>
2026-07-25 00:15:22 +0800

    keep going

--- practices/Day25.swift
+++ practices/Day25.swift
@@ -1,7 +1,7 @@
 enum studentType {
-    case Regular
-    case Irregular 
-    case PartTime
+    case IT
+    case NS
+    case CTIHM
 }
 
 enum Subjects {
@@ -9,13 +9,24 @@ enum Subjects {
     case Research
     case Professionalism
 }
+
+extension studentType  {
+    var properUnit: Subjects {
+        switch self {
+            case .IT: return .Programming
+            case .NS: return .Research
+            case .CTIHM: return .Professionalism
+        }
+
+    }
+}
 struct students {
     var name: String
     var year: Int?
-    var course: String
+    var type: studentType
 
     var description: String {
-        return "\(name), \(year!) ,\(course)"
+        return "\(name), \(year!) ,\(type)"
     }
     mutating func Architect(for arch:students) {
         if arch.year! > 4 {
@@ -36,12 +47,16 @@ struct students {
 }
 
 var studentDB: [students] = [
-    students(name: "Ralph", year: 3 , course: "BSIT"),
-    students(name: "Clark", year: 3 , course: "BSNS"),
-    students(name: "Lance", year: 5 , course: "BSARCH"),
-    students(name: "John" , year:nil, course: "BSIT")
+    students(name: "Ralph", year: 3 , type: .IT),
+    students(name: "Clark", year: 3 , type: .IT),
+    students(name: "Lance", year: 5 , type: .NS),
+    students(name: "John" , year:nil, type: .CTIHM)
 ]
 
 print(studentDB[0].description)  
 studentDB[2].Architect(for: studentDB[2])
 studentDB[3].validation(for: studentDB[3])
+
+
+let units = studentType.IT 
+print(units)

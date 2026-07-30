65326a57e8232218f2072146179f94d55ef529b6
Ralph <gonzagaralphdainiell@gmail.com>
2026-07-30 23:20:30 +0800

    Keep going

---
+++ practices/Day31.swift
@@ -0,0 +1,29 @@
+enum Category {
+    case electronics,clothing,food
+}
+
+struct Product {
+    var name: String
+    var price: Double
+    var category: Category 
+
+}
+struct Inventory {
+    var products: [Product]
+
+    
+    func expensive(thresold: Double) -> [Product] {
+        return products.filter {$0.price >= thresold}
+    }
+    func allNames() -> [String]{
+        return products.map {$0.name}
+    }
+    func totalPrice() -> Double {
+        return products.reduce(0) {$0 + $1.price}
+    }
+
+    init(products: [Product]) {
+        self.products = products
+    }
+}
+    

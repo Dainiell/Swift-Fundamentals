79474b3e588d9ca892c68b91ebe6e1430383a646
Ralph <gonzagaralphdainiell@gmail.com>
2026-08-04 22:26:14 +0800

    Keep going

--- practices/Day36.swift
+++ practices/Day36.swift
@@ -18,7 +18,37 @@ struct Employee {
 
 var CompanyDB: [Employee] = [
     Employee(name: "Lance", DateHire: "June 12,2029" , RoleType: .Junior , Position: .Database),
-    Employee(name: "Ralph", DateHire: "June 8,2029" , RoleType: .Junior , Position: .Developer)
+    Employee(name: "Ralph", DateHire: "June 8,2029" , RoleType: .Junior , Position: .Developer),
+    Employee(name: "Dainiell", DateHire: "July 14,2029" , RoleType: .Junior , Position: .Developer)
+
+
 ]
 
 print(CompanyDB[1].iDFormat)
+
+func DatabaseAnalyzer() -> [Employee] {
+    var DB: [Employee] = [] 
+
+    for positions in CompanyDB {
+        if positions.Position == .Database {
+            DB.append(positions)
+        }
+    }
+    return DB
+}
+let analyzer1 = DatabaseAnalyzer() 
+print("\(analyzer1.count) - Database Employee")
+
+
+func DevAnalyzer() -> [Employee] {
+    var Dev: [Employee] = [] 
+
+    for positions in CompanyDB {
+        if positions.Position == .Developer {
+            Dev.append(positions)
+        }
+    }
+    return Dev
+}
+let analyzer2 = DevAnalyzer() 
+print("\(analyzer2.count) - Developer Employee")

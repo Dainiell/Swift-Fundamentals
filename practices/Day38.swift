4c71d7e0a939a09c67065e64138068e9587037df
Ralph <gonzagaralphdainiell@gmail.com>
2026-08-07 23:27:42 +0800

    Keep going

--- practices/Day38.swift
+++ practices/Day38.swift
@@ -1,16 +1,22 @@
 class GymMember {
     var name: String 
     var MembershipType: String 
+    var visits: Int
 
     init(name: String, MembershipType: String) {
         self.name = name
         self.MembershipType = MembershipType
+        self.visits = 0
     }
 
     func checkIn() -> String {
-        return "\(name) checked in as a \(MembershipType) member"
+        visits += 1
+        return "\(name) checked in as a \(MembershipType) member (Visit #\(visits))"
     } 
 
+        func addBonusVisits(_ count: Int) {
+        visits += count
+    }
 }
 
 let member1 = GymMember(name: "Ralph" , MembershipType: "Premium") 
@@ -18,3 +24,6 @@ print(member1.checkIn())
 
 member1.MembershipType = "Regular"
 print(member1.checkIn()) 
+
+member1.addBonusVisits(0)
+print(member1.checkIn())
